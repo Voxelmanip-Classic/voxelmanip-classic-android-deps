86d913de4f6927df5b0b3447ce6a668458b6f01e
ROllerozxa <rollerozxa@voxelmanip.se>
2023-05-26 17:17:15 +0200

    Remove unneeded deps, switch to irrlicht-vmc, disable building for x86 and x86_64

--- scripts/Irrlicht.sh
+++ scripts/Irrlicht.sh
@@ -1,44 +1,33 @@
 #!/bin/bash -e
 irrlicht_ver=1.9.0mt10
 png_ver=1.6.37
-jpeg_ver=2.1.4
 
 download () {
 	if [ ! -d irrlicht/.git ]; then
-		git clone https://github.com/minetest/irrlicht irrlicht
-		pushd irrlicht
-		git checkout $irrlicht_ver
-		popd
+		git clone https://github.com/rollerozxa/irrlicht-vmc irrlicht
+		#pushd irrlicht
+		#git checkout $irrlicht_ver
+		#popd
 	fi
 	get_tar_archive libpng "https://download.sourceforge.net/libpng/libpng-${png_ver}.tar.gz"
-	get_tar_archive libjpeg "https://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-${jpeg_ver}.tar.gz"
 }
 
 build () {
-	# Build libjpg and libpng first because Irrlicht needs them
+	# Build libpng first because Irrlicht needs it
 	mkdir -p libpng
 	pushd libpng
 	$srcdir/libpng/configure --host=$CROSS_PREFIX
 	make && make DESTDIR=$PWD install
 	popd
 
-	mkdir -p libjpeg
-	pushd libjpeg
-	cmake $srcdir/libjpeg "${CMAKE_FLAGS[@]}" -DENABLE_SHARED=OFF
-	make && make DESTDIR=$PWD install
-	popd
-
 	local libpng=$PWD/libpng/usr/local/lib/libpng.a
-	local libjpeg=$(echo $PWD/libjpeg/opt/libjpeg-turbo/lib*/libjpeg.a)
 	cmake $srcdir/irrlicht "${CMAKE_FLAGS[@]}" \
 		-DBUILD_SHARED_LIBS=OFF \
 		-DPNG_LIBRARY=$libpng \
-		-DPNG_PNG_INCLUDE_DIR=$(dirname "$libpng")/../include \
-		-DJPEG_LIBRARY=$libjpeg \
-		-DJPEG_INCLUDE_DIR=$(dirname "$libjpeg")/../include
+		-DPNG_PNG_INCLUDE_DIR=$(dirname "$libpng")/../include
 	make
 
-	cp -p lib/Android/libIrrlichtMt.a $libpng $libjpeg $pkgdir/
+	cp -p lib/Android/libIrrlichtMt.a $libpng $pkgdir/
 	cp -a $srcdir/irrlicht/include $pkgdir/include
 	cp -a $srcdir/irrlicht/media/Shaders $pkgdir/Shaders
 }
